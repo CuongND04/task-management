4a7fe4db3b8d110f579bf86354694c681b0ef795
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-24 23:32:18 +0700

    9. Sắp xếp theo tiêu chí

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -2,12 +2,21 @@ const Task = require("../models/task.model");
 
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
+  // lọc theo trạng thái
   const find = { deleted: false };
   if (req.query.status) {
     // nếu có status sau dấu "?"
     find.status = req.query.status;
   }
-  const tasks = await Task.find(find);
+  // Sắp xếp
+  const sort = {};
+
+  if (req.query.sortKey && req.query.sortValue) {
+    // chỗ này có "[]" vì tên thuộc tính của object có thể có nhiều hơn 2 từ
+    sort[req.query.sortKey] = req.query.sortValue;
+  }
+
+  const tasks = await Task.find(find).sort(sort);
 
   res.json(tasks);
 };
