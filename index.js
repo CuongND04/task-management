641438d87c05391b4947baec9a5b17e0e641e762
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-24 16:00:07 +0700

    6.1. Tạo routes

--- index.js
+++ index.js
@@ -5,27 +5,9 @@ const app = express();
 const port = process.env.PORT;
 
 database.connect();
-const Task = require("./models/task.model");
 
-app.get("/tasks", async (req, res) => {
-  const tasks = await Task.find({
-    deleted: false,
-  });
-  res.json(tasks);
-});
-
-app.get("/tasks/detail/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const task = await Task.findOne({
-      _id: id,
-      deleted: false,
-    });
-    res.json(task);
-  } catch (error) {
-    res.json("Not Found");
-  }
-});
+const routesApiV1 = require("./api/v1/routes/index.route");
+routesApiV1(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
