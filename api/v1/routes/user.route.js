12487d94cf6d9eff2cb20dfd50e27d14f192b4d0
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-25 03:30:01 +0700

    1. Tính năng đăng nhập

--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -5,4 +5,5 @@ const controller = require("../controllers/user.controller.js");
 
 router.post("/register", controller.register);
 
+router.post("/login", controller.login);
 module.exports = router;
